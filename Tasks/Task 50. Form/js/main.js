9e128a14f2e0ccd5653b0e9db54c9ca5de5c1764
Alex Nikitin <ip.alexnikitin@gmail.com>
2023-02-28 21:27:56 +0300

    Task 50. Solve

--- Tasks/Task 50. Form/js/main.js
+++ Tasks/Task 50. Form/js/main.js
@@ -3,24 +3,89 @@ let form = document.getElementById("reg_form");
 form.addEventListener("submit", function(event){
     event.preventDefault(); // отменяет стандартные действия браузера
 
-    let div = document.querySelector(".result");
+    // <span class="error-text">Заполните поле</span>
 
-    let name = document.querySelector("#name").value;   
-    div.innerHTML = div.innerHTML + `Имя: ${name}<br>`;
+    let errors = document.querySelectorAll(".error-text");
 
-    let email = document.querySelector("#email").value;
-    div.innerHTML += `Email: ${email}<br>`;
-
-    let phone = document.querySelector("#phone").value;
-    div.innerHTML += `Телефон: ${phone}<br>`;
-
-    let education = document.querySelector("#education").value;
-    div.innerHTML += `Образование: ${education}<br>`;
-
-    let inputHobby1 = document.querySelector("#hobby-1");
-
-    if(inputHobby1.checked){ // проверяет, выбранный ли данный пункт (стоит ли галочка)
-        div.innerHTML += `Хобби: ${inputHobby1.value}<br>`;
+    if(errors.length){
+        Array.from(errors).forEach((errorSpan) => {
+            errorSpan.parentElement.classList.remove("error");
+            errorSpan.remove();
+        })
     }
 
+    let hasError = false;
+
+    let nameInput = document.querySelector("#name");
+    let emailInput = document.querySelector("#email");
+    let phoneInput = document.querySelector("#phone");
+    let aboutInput = document.querySelector("#about");
+    let educationInput = document.querySelector("#education");
+
+    let fields = [nameInput, emailInput, phoneInput, aboutInput, educationInput];
+
+    fields.forEach((field) => {
+        if(field.value == ""){
+            let span = document.createElement("span");
+            span.className = "error-text"; // span.classList.add("error-text");
+            span.innerText = "Заполните поле";
+            field.insertAdjacentElement("afterend", span);
+            field.parentElement.classList.add("error");
+            hasError = true;
+        }
+    });
+
+    let sexs = ["sex-m", "sex-f"];
+
+    let checked = sexs.some((radioId) => {
+        return document.querySelector("#"+radioId).checked; //document.querySelector("#"+radioId) - input
+    })
+
+    if(!checked){
+        let span = document.createElement("span");
+        span.className = "error-text"; // span.classList.add("error-text");
+        span.innerText = "Выберите пол";
+        document.querySelector("#sex-f").parentElement.insertAdjacentElement("afterend", span);
+        hasError = true;
+    }
+
+    let hobbies = ["hobby-1", "hobby-2", "hobby-3"];
+
+    checked = hobbies.some((checkboxId) => {
+        return document.querySelector("#"+checkboxId).checked; //document.querySelector("#"+checkbox) - input
+    })
+
+    if(!checked){
+        let span = document.createElement("span");
+        span.className = "error-text"; // span.classList.add("error-text");
+        span.innerText = "Выберите хобби";
+        document.querySelector("#hobby-3").parentElement.insertAdjacentElement("afterend", span);
+        hasError = true;
+    }
+
+    if(!hasError){
+        let div = document.querySelector(".result");
+
+        div.innerHTML += `Имя: ${nameInput.value}<br>`;
+        div.innerHTML += `Email: ${emailInput.value}<br>`;
+        div.innerHTML += `Телефон: ${phoneInput.value}<br>`;
+        div.innerHTML += `Образование: ${educationInput.value}<br>`;
+        if(document.querySelector("#sex-m").checked){
+            div.innerHTML += `Пол: ${document.querySelector("#sex-m").value}<br>`;
+        } else if(document.querySelector("#sex-f").checked){
+            div.innerHTML += `Пол: ${document.querySelector("#sex-f").value}<br>`;
+        } 
+
+        let hobbyValue = [];
+
+        hobbies.forEach((hobby) => {
+            if(document.querySelector("#"+hobby).checked){
+                hobbyValue.push(document.querySelector("#"+hobby).value);
+            }
+        })
+
+        div.innerHTML += `Хобби: ${hobbyValue.join(",")}<br>`;
+        div.innerHTML += `О себе: ${aboutInput.value}<br>`;
+        this.reset(); // очищение формы this==form
+    }
 })
